0bd05324c53d95308b9d3f9a6374e8466707024b
Manthan Sharma <techmanthansharma@gmail.com>
2024-11-01 15:05:34 +0530

    add typeorm

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,21 @@
 import dotenv from "dotenv";
+import path from "node:path";
 
-dotenv.config();
+dotenv.config({
+  path: path.join(__dirname, `../../.env.${process.env.NODE_ENV}`),
+});
 
-const { PORT, NODE_ENV } = process.env;
+const { PORT, NODE_ENV, DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_NAME } =
+  process.env;
 
 const CONFIG = Object.freeze({
   PORT,
   NODE_ENV,
+  DB_HOST,
+  DB_PORT,
+  DB_USERNAME,
+  DB_PASSWORD,
+  DB_NAME,
 });
 
 export default CONFIG;
---
+++ src/database/data-source.ts
@@ -0,0 +1,18 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { User } from "../entity/User";
+import CONFIG from "../config";
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: CONFIG.DB_HOST,
+  port: Number(CONFIG.DB_PORT),
+  username: CONFIG.DB_USERNAME,
+  password: CONFIG.DB_PASSWORD,
+  database: CONFIG.DB_NAME,
+  synchronize: CONFIG.NODE_ENV === "test" || CONFIG.NODE_ENV === "dev",
+  logging: false,
+  entities: [User],
+  migrations: [],
+  subscribers: [],
+});
