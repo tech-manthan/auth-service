028036451933f7ac79d07071acd40ce8bc136e6c
Manthan Sharma <techmanthansharma@gmail.com>
2024-11-09 00:55:27 +0530

    add test coverage

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,7 @@ module.exports = {
     "^.+.tsx?$": ["ts-jest", {}],
   },
   verbose: true,
+  collectCoverage: true,
+  coverageProvider: "v8",
+  collectCoverageFrom: ["src/**/*.ts", "!tests/**", "!**/node_module/**"],
 };
